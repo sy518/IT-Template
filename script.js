2f362fe85c5fddd3a4520c4d03712ccde300a909
sy518 <samanazehra78692@gmail.com>
2025-04-10 22:04:32 -0700

    fist commit

---
+++ script.js
@@ -0,0 +1,329 @@
+let arry1 = [2, 4, 8, 6, 10];
+for (let i = 0; i < arry1.length; i++) {
+    const element = arry1[i];
+    console.log(element);
+
+}
+
+let arry2 = ["allu", "bhalu", 'khalo',];
+for (let i = 0; i < arry2.length; i++) {
+    const ele = arry2[i];
+    console.log(ele);
+
+}
+let abc3 = ["hi", "hello", "gee"];
+for (let i = 0; i < abc3.length; i++) {
+    const efg = abc3[i];
+    console.log(efg);
+
+
+}
+let a4 = [33, 44, 55, 66, 77];
+for (let i = 0; i < a4.length; i++) {
+    const a2 = a4[i]
+    console.log(a2);
+
+}
+let a5 = [11, 33, 44, 55, 66, 77, 88];
+for (let i = 0; i < a5.length; i++) {
+    const as = a5[i];
+    console.log(as);
+
+
+}
+let b6 = [55, 12, 23, 445];
+for (let i = 0; i < b6.length; i++) {
+    const el = b6[i];
+    console.log(el);
+
+
+}
+let b7 = [1, 2, 3, 4];
+for (let i = 0; i < b7.length; i++) {
+    const b6 = b7[i];
+    console.log(b6);
+
+
+}
+let a8 = [1, 2, 3]
+for (let i = 0; i < a8.length; i++) {
+    const ment = a8[i];
+    console.log(ment);
+
+
+}
+let a9 = [1, 2, 3]
+for (let i = 0; i < a9.length; i++) {
+    const nt = a9[i];
+    console.log(nt);
+
+
+}
+let a10 = [4, 5, 66]
+for (let i = 0; i < a10.length; i++) {
+    const off = a10[i];
+    console.log(off);
+}
+let a11 = [6, 7, 8]
+for (let i = 0; i < a11.length; i++) {
+    const of = a11[i];
+    console.log(of);
+
+
+}
+let a12 = [90, 8, 7, 6]
+for (let i = 0; i < a12.length; i++) {
+    const wo = a12[i];
+    console.log(wo);
+
+}
+let a13 = [3, 5, 6]
+for (let i = 0; i < a13.length; i++) {
+    const ho = a13[i];
+    console.log(ho);
+
+
+}
+let a14 = [3, 4, 56, 6]
+for (let i = 0; i < a14.length; i++) {
+    const so = a14[i];
+    console.log(so);
+
+
+}
+let a15 = [3, 45, 6, 0]
+for (let i = 0; i < a15.length; i++) {
+    const eo = a15[i];
+    console.log(eo);
+
+}
+let a16 = [2, 3, 4];
+for (let i = 0; i < a16.length; i++) {
+    const to = a16[i];
+    console.log(to);
+
+
+}
+let a17 = [1, 24, 67]
+for (let i = 0; i < a17.length; i++) {
+    const no = a17[i];
+    console.log(no);
+
+
+}
+let a18 = [2, 4, 6]
+for (let i = 0; i < a18.length; i++) {
+    const mo = a18[i];
+    console.log(mo);
+
+
+}
+let a19 = [3, 5, 7]
+for (let i = 0; i < a19.length; i++) {
+    const elo = a19[i];
+    console.log(elo);
+
+
+}
+let a20 = [2, 5, 6]
+for (let i = 0; i < a20.length; i++) {
+    const mano = a20[i];
+    console.log(mano);
+
+}
+let a21 = [2, 4, 6]
+for (let i = 0; i < a21.length; i++) {
+    const mi = a21[i];
+    console.log(mi);
+
+
+}
+let a22 = [2, 4, 5]
+for (let i = 0; i < a22.length; i++) {
+    const jhfaj = a22[i];
+    console.log(jhfaj);
+
+
+}
+let a23 = [2, 3, 4]
+for (let i = 0; i < a23.length; i++) {
+    const asf = a23[i];
+    console.log(asf);
+
+}
+let a24 = [2, 4, 5]
+for (let i = 0; i < a24.length; i++) {
+    const mm = a24[i];
+    console.log(mm);
+
+
+}
+let a25 = [2, 3, 4]
+for (let i = 0; i < a25.length; i++) {
+    const ec = a25[i];
+    console.log(ec);
+
+}
+let a26 = [3, 5, 6]
+for (let i = 0; i < a26.length; i++) {
+    const nn = a26[i];
+    console.log(nn);
+
+}
+let a27 = [2, 24, 3]
+for (let i = 0; i < a27.length; i++) {
+    const vv = a27[i];
+    console.log(vv);
+
+}
+let a28 = [2, 35, 5]
+for (let i = 0; i < a28.length; i++) {
+    const cc = a28[i];
+    console.log(cc);
+
+
+}
+let a29 = [7, 88, 9]
+for (let i = 0; i < a29.length; i++) {
+    const ai = a29[i];
+    console.log(ai);
+
+
+}
+let a30 = [9, 15, 3]
+for (let i = 0; i < a30.length; i++) {
+    const bl = a30[i];
+    console.log(bl);
+
+
+}
+let a31 = [2, 54, 6]
+for (let i = 0; i < a31.length; i++) {
+    const tu = a31[i];
+    console.log(tu);
+
+
+}
+let a32 = [8, 19, 3]
+for (let i = 0; i < a32.length; i++) {
+    const mo = a32[i];
+    console.log(mo);
+
+
+}
+let a33 = [6, 7, 8]
+for (let i = 0; i < a33.length; i++) {
+    const th = a33[i];
+    console.log(th);
+
+}
+let a34 = [2, 4, 5]
+for (let i = 0; i < a34.length; i++) {
+    const tr = a34[i];
+    console.log(tr);
+
+}
+let a35 = [6, 7, 8]
+for (let i = 0; i < a35.length; i++) {
+    const ba = a35[i];
+    console.log(ba);
+
+}
+let a36 = [2, 4, 5]
+for (let i = 0; i < a36.length; i++) {
+    const ed = a36[i];
+    console.log(ed);
+
+}
+let a37 = [2, 3, 4];
+for (let i = 0; i < a37.length; i++) {
+    const aa = a37[i];
+    console.log(aa);
+
+
+}
+let a38 = [1, 24, 67]
+for (let i = 0; i < a38.length; i++) {
+    const nu = a38[i];
+    console.log(nu);
+
+
+}
+let a39 = [2, 4, 6]
+for (let i = 0; i < a39.length; i++) {
+    const oo = a39[i];
+    console.log(oo);
+
+
+}
+let a40 = [3, 5, 7]
+for (let i = 0; i < a40.length; i++) {
+    const lo = a40[i];
+    console.log(lo);
+
+
+}
+let a41 = [2, 5, 6]
+for (let i = 0; i < a41.length; i++) {
+    const mano = a41[i];
+    console.log(mano);
+
+}
+let a42 = [2, 4, 6]
+for (let i = 0; i < a42.length; i++) {
+    const me = a42[i];
+    console.log(me);
+
+
+}
+let a43 = [2, 4, 5]
+for (let i = 0; i < a43.length; i++) {
+    const jh = a43[i];
+    console.log(jh);
+
+
+}
+let a44 = [2, 3, 4]
+for (let i = 0; i < a44.length; i++) {
+    const sf = a44[i];
+    console.log(sf);
+
+}
+let a45 = [2, 4, 5]
+for (let i = 0; i < a45.length; i++) {
+    const ll = a45[i];
+    console.log(ll);
+
+
+}
+let a46 = [2, 3, 4]
+for (let i = 0; i < a46.length; i++) {
+    const dd = a46[i];
+    console.log(dd);
+
+}
+let a47 = [3, 5, 6]
+for (let i = 0; i < a47.length; i++) {
+    const kk = a47[i];
+    console.log(kk);
+
+}
+let a48 = [2, 24, 3]
+for (let i = 0; i < a48.length; i++) {
+    const ii = a48[i];
+    console.log(ii);
+
+}
+let a49 = [2, 35, 5]
+for (let i = 0; i < a49.length; i++) {
+    const ff = a49[i];
+    console.log(ff);
+
+
+}
+let a50 = [3, 6, 8]
+for (let i = 0; i < a50.length; i++) {
+    const gg = a50[i];
+    console.log(gg);
+
+}    
